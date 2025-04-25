d3e2527c641eac8fa35e260b538e60b7707a67b3
Leonardo Ricci Santos Oliveira <Lo5642050@gmail.com>
2025-04-25 14:29:59 -0300

    feat: Configurando Rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { CadastrarComponent } from './components/cadastrar/cadastrar.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"", component: LoginComponent
+    },
+    {
+        path:"cadastrar", component: CadastrarComponent
+    },
+    {
+        path:"home", component:HomeComponent
+    }
+]
