134f5f7ffc976fff7be58dcc9a6781f9bc9b5143
Leonardo Ricci Santos Oliveira <Lo5642050@gmail.com>
2025-05-09 09:14:58 -0300

    Atualiza componente Home com diretivas e layout final

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css'],
+  standalone: true, 
+  imports: [CommonModule],
 })
 export class HomeComponent {
+  termos = [
+    { nome: '1º Termo', cor: '#DFF6C4' },
+    { nome: '2º Termo', cor: '#EDE5DA' },
+    { nome: '3º Termo', cor: '#A6D8A8' },
+    { nome: '4º Termo', cor: '#BCE9A8' },
+    { nome: '5º Termo', cor: '#FFFBCC' },
+    { nome: '6º Termo', cor: '#A8B6A6' }
+  ];
 
+  adicionarTurma() {
+    alert('Função de adicionar turma acionada!');
+  }
+
+  acessarTermo(termo: string) {
+    alert(`Você clicou em ${termo}`);
+  }
 }
+
